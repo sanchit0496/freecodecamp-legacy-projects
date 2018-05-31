eea120f79ebd432b95abb822b5cb6a595a3a47ef
Sanchit <sanchit0496@gmail.com>
2018-05-31 15:17:53 +0530

    freeCodecamp Wikipedia Viewer API Project

---
+++ WikipediaViewerAPI/index.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  
+  
+    $('#search').click(function(){
+    
+      var searchTerm = $('#searchTerm').val();
+      
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&callback=?"
+      
+      $.ajax({
+        type : "GET",
+        url : url,
+        async:false,
+        dataType:'json',
+        success: function(data){
+          console.log(data[2][1]);
+          
+          
+       $('#results').html(''); //clears all data prior to running/re-running for loop   
+          
+          
+       for (var i = 0; i < data[1].length; i++) {
+            
+          $('#results').append("<a href=" + data[3][i] +"><h1>" + data[1][i] + "</h1></a>" + "<h3>" + data[2][i] + "</h3><br>");
+          }
+                   
+  
+          
+          
+      $("#searchTerm").val('');        
+        },
+        error: function(error){
+          alert("Error")
+        },
+      
+      
+      
+      
+      
+      
+      });
+      
+      
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    });
+    
+    
+    
+    
+  });
+                    
+                    
