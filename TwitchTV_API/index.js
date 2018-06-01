b95df17c91bf9104940a7fb4e234c3f3ec0f2b0b
Sanchit <sanchit0496@gmail.com>
2018-06-02 00:54:07 +0530

    Adding the jQuery JS File

---
+++ TwitchTV_API/index.js
@@ -0,0 +1,56 @@
+//Client ID= 59x9ex7f5zzongzntqx0zrwleoxy12  slice 37
+
+$(function() {
+
+  var follower = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.twitch.tv/kraken/streams/freecodecamp', //change this to test active
+    headers: {
+      'client-ID': '7fuu18v0p6bs5m0pvlg4upv4n3965b'
+    },
+    success: function(data) {
+      console.log(data);
+      if (data.stream === null) {
+        $('#fcc').html(' is offline');
+      } else {
+        $('#fcc').html(' is ONLINE!');
+      }
+    },
+    error: function(err) {
+      alert("Error");
+    }
+  });
+
+  for (var i = 0; i < follower.length; i++) {
+
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.twitch.tv/kraken/streams/' + follower[i], //change this to test active
+      headers: {
+        'client-ID': '7fuu18v0p6bs5m0pvlg4upv4n3965b'
+      },
+      success: function(dataI) {
+
+        var name = dataI._links.self.slice(37)
+        console.log(dataI);
+        
+        if (dataI.stream === null) {
+          $('#status').append(' is offline<br>');
+          $('#user').append('<a target="blank" href="https://www.twitch.tv/' + name + '">' + name + '</a><br>')
+          $('#game').append('N/A<br>');
+        } else {
+          $('#status').append(' is ONLINE!<br>');
+          $('#user').append('<a target="blank" href="https://www.twitch.tv/' + name + '">' + name + '</a><br>')
+          $('#game').append(dataI.stream.game + '<br>');
+        }
+      },
+      error: function(err) {
+        alert("Error: One or more users is no longer avaialble");
+      }
+    });
+
+  }
+
+})
