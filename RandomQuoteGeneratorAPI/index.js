9ebd02a1247c0dd7b4376bef7152037edb95a143
Sanchit <sanchit0496@gmail.com>
2018-06-02 14:37:20 +0530

    Quote Generator using API and jQuery

---
+++ RandomQuoteGeneratorAPI/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  
+    getQuote()
+    
+    var quote
+    var author
+    
+    function getQuote(){
+    
+      var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+  
+      $.getJSON(url, function(data){
+      $("#quote").html('"'+data.quoteText+'"'); 
+      $("#author").html("-"+data.quoteAuthor);
+    });
+      
+      $("#newQuote").on("click", function(){
+      getQuote();
+    });
+      
+      
+    }  
+    
+    
+    
+    
+  });
